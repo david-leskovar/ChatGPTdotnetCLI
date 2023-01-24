af12180fce1bbe054b0912bd07d106465cc11c0e
David Leskovar <david.leskovar98@gmail.com>
2023-01-24 22:53:01 +0100

    ChatGPTdotnetCLI

---
+++ ChatGPTConsoleApp/Program.cs
@@ -0,0 +1,108 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+
+namespace ChatGPT
+{
+    public class Program
+    {
+        public static string Guess(string raw)
+        {
+            Console.WriteLine("---> GPT-3 API Returned Text:");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(raw);
+
+
+            var lastIndex = raw.LastIndexOf('\n');
+            string guess = raw.Substring(lastIndex + 1);
+
+            Console.ResetColor();
+
+            TextCopy.ClipboardService.SetText(guess);
+
+
+            return guess;
+        }
+
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter your API key");
+            string apikey = Console.ReadLine();
+
+            
+
+            while (true)
+            {
+
+                Console.WriteLine("Ask a question");
+                string question  = Console.ReadLine();
+
+
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("authorization",
+                    $"Bearer {apikey}");
+
+                var content = new StringContent(
+                    "{\"model\": \"text-davinci-001\", \"prompt\": \"" + question +
+                    "\",\"temperature\": 1,\"max_tokens\": 100}",
+                    Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+                string responseString = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    ResponseError? res = JsonSerializer.Deserialize<ResponseError>(responseString);
+
+
+                    string guess = Guess(res.error.message);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"---> My guess at the command prompt is:  {guess}");
+                    Console.ResetColor();
+                    continue;
+                }
+
+                try
+                {
+                    var data = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+                    string guess = Guess(data!.choices[0].text);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"---> My guess at the command prompt is:  {guess}");
+                    Console.ResetColor();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+
+            }
+
+
+
+            
+        }
+
+
+        public class ResponseError
+        {
+            public Error error { get; set; }
+        }
+
+        public class Error
+        {
+            public string? message { get; set; }
+            public string? type { get; set; }
+            public string? param { get; set; }
+            public string? code { get; set; }
+        }
+    }
+}
